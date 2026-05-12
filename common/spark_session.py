1c7bcbc689b9d79c17d41c81ccde851598170eff
Grey <permale.ra.tech@gmail.com>
2026-05-13 09:44:17 +1000

    fixed port misfire

--- common/spark_session.py
+++ common/spark_session.py
@@ -1,7 +1,9 @@
 def get_spark():
     try:
         from databricks.connect import DatabricksSession
-        return DatabricksSession.builder.getOrCreate()
+        from databricks.sdk.core import Config
+        config = Config(profile="DEFAULT")
+        return DatabricksSession.builder.sdkConfig(config).getOrCreate()
     except ImportError:
         from pyspark.sql import SparkSession
         return SparkSession.builder.getOrCreate()
